d01d5a2befba979b3e59c98a535c61e0d63f7d94
Nur A Alam <nuraalam376@gmail.com>
2021-02-13 22:12:37 +0600

    practiced attribute binding

--- vue-firebase/app.js
+++ vue-firebase/app.js
@@ -7,6 +7,30 @@ const app = Vue.createApp({
       age: 45,
       x: 0,
       y: 0,
+      books: [
+        {
+          title: "The book of Abc",
+          author: "Abc",
+          img:
+            "https://thewritelife.com/wp-content/uploads/2019/08/How-to-format-a-book.jpg",
+          fav: true,
+        },
+        {
+          title: "The book of Def",
+          author: "Def",
+          img:
+            "https://thewritelife.com/wp-content/uploads/2019/08/How-to-format-a-book.jpg",
+          fav: false,
+        },
+        {
+          title: "The book of Ghi",
+          author: "Ghi",
+          img:
+            "https://thewritelife.com/wp-content/uploads/2019/08/How-to-format-a-book.jpg",
+          fav: true,
+        },
+      ],
+      url: "https://www.google.com",
     };
   },
   methods: {
@@ -25,6 +49,15 @@ const app = Vue.createApp({
     handleMouseMove(e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
+    },
+    toggleImage(book) {
+      book.fav = !book.fav;
+    },
+  },
+
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.fav);
     },
   },
 });
