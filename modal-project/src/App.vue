18cd790fcec57d3bd3447e2d8d67384112772747
Nur A Alam <nuraalam376@gmail.com>
2021-02-14 13:24:03 +0600

    started learning vue.js modal project

---
+++ modal-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Modal />
+  </div>
+</template>
+
+<script>
+import Modal from "./components/Modal";
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "First Vue App",
+    };
+  },
+
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
