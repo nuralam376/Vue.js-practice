519bf22062b536714c8d998bd89aca764293e12f
Nur A Alam <nuraalam376@gmail.com>
2021-02-19 09:30:10 +0600

    started working on reaction timer

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Playing</h1>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+};
+</script>
+
+<style scoped>
+h1 {
+  width: 400px;
+  padding: 100px 0;
+  text-align: center;
+  background: green;
+  margin: 50px auto;
+}
+</style>
