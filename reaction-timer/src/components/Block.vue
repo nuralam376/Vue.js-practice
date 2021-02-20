f9ecc3cbdd7481ebdd9ece0393b038d4987ed659
Nur A Alam <nuraalam376@gmail.com>
2021-02-20 10:04:00 +0600

    practiced vue.js hooks

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,10 +1,38 @@
 <template>
-  <h1>Playing</h1>
+  <h1 v-if="showBlock" @click="stopTimer">Playing</h1>
 </template>
 
 <script>
 export default {
   props: ["delay"],
+
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log("Timer", this.reactionTime);
+    },
+  },
 };
 </script>
 
