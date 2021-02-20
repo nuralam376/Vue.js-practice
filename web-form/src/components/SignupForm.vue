725ec1726b84e4223118d5380f70c7cb54675e04
Nur A Alam <nuraalam376@gmail.com>
2021-02-20 22:20:00 +0600

    practiced vue.js form validation

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -1,32 +1,38 @@
 <template>
-  <label for="email">Email</label>
-  <input type="email" required v-model="email" />
-  <br />
-  <label for="password">Password</label>
-  <input type="password" required v-model="password" />
-  <br />
+  <form action="" @submit.prevent="handleSubmit">
+    <label for="email">Email</label>
+    <input type="email" required v-model="email" />
+    <br />
+    <label for="password">Password</label>
+    <input type="password" required v-model="password" />
+    <p v-if="passwordError">{{ passwordError }}</p>
+    <br />
 
-  <select name="" id="" v-model="role">
-    <option value="designer">Web Designer</option>
-    <option value="developer">Web Developer</option>
-  </select>
+    <select name="" id="" v-model="role">
+      <option value="designer">Web Designer</option>
+      <option value="developer">Web Developer</option>
+    </select>
 
-  <br />
-  <input type="checkbox" v-model="terms" />
-  <label for="terms">Accept terms and conditions</label>
-  <br />
-  <label for="name">Abc</label>
-  <input type="checkbox" value="abc" v-model="names" />
-  <br />
-  <label for="def">Def</label>
-  <input type="checkbox" value="def" v-model="names" />
-  <br />
+    <br />
+    <input type="checkbox" v-model="terms" />
+    <label for="terms">Accept terms and conditions</label>
+    <br />
+    <label for="name">Abc</label>
+    <input type="checkbox" value="abc" v-model="names" />
+    <br />
+    <label for="def">Def</label>
+    <input type="checkbox" value="def" v-model="names" />
+    <br />
 
-  <label for="ghi">Ghi</label>
-  <input type="checkbox" value="ghi" v-model="names" />
-  <br />
+    <label for="ghi">Ghi</label>
+    <input type="checkbox" value="ghi" v-model="names" />
+    <br />
+    <input type="submit" />
+  </form>
   <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
-  <p v-for="skill in skills" :key="skill">{{ skill }}</p>
+  <p v-for="skill in skills" :key="skill">
+    <span @click="deleteSkill(skill)">{{ skill }}</span>
+  </p>
   <br />
 
   <p>Email - {{ email }}</p>
@@ -47,6 +53,7 @@ export default {
       skills: [],
       tempSkill: "",
       terms: "",
+      passwordError: "",
     };
   },
 
@@ -58,6 +65,16 @@ export default {
         }
       }
       this.tempSkill = "";
+    },
+
+    deleteSkill(item) {
+      console.log(item);
+      this.skills = this.skills.filter((skill) => skill !== item);
+    },
+
+    handleSubmit() {
+      console.log("Form Submitted");
+      this.passwordError = this.password.length > 5 ? "" : "Invalid password";
     },
   },
 };
