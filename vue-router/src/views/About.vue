a1e67360c7e5acf3b67163368cf702df904af97e
Nur A Alam <nuraalam376@gmail.com>
2021-02-21 10:14:05 +0600

    started learning vue router

---
+++ vue-router/src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>About page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quam
+      eligendi alias asperiores ab, animi laboriosam qui veritatis harum iusto
+      laudantium repudiandae necessitatibus tempore ad. Dicta quaerat,
+      consequuntur deleniti qui nulla rerum eligendi cum repellendus maiores.
+      Iure, unde magnam consequatur dolorem possimus sit dicta aliquam accusamus
+      ducimus minus, exercitationem sapiente ratione similique sed repudiandae
+      voluptates ex quidem harum dolores accusantium reiciendis voluptatem? Nam
+      magni id soluta libero ad quae eaque molestiae, itaque quo deserunt ab!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ex
+      molestiae perspiciatis officiis hic iusto rerum omnis quaerat quia,
+      consequuntur reiciendis itaque voluptates deserunt dolorem.
+    </p>
+  </div>
+</template>
---
+++ vue-router/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos
+      distinctio perferendis nostrum ipsum animi maiores nam ullam laborum
+      blanditiis autem, nulla, necessitatibus vitae deserunt natus porro. Magnam
+      minima asperiores perspiciatis ipsum nemo delectus odio, beatae nulla
+      illum adipisci maxime pariatur molestias veritatis porro quae autem,
+      earum, tenetur aliquam aspernatur nostrum voluptates similique. Nam a
+      velit aliquid, cumque consequatur sed soluta eos quod quasi quibusdam
+      harum, minus et incidunt beatae aspernatur cupiditate quas ipsum
+      blanditiis. Repellendus officiis error sunt ipsa mollitia maxime sapiente
+      blanditiis voluptas, assumenda optio sed modi eius aliquid! Ipsam error
+      quidem consectetur accusamus laboriosam tenetur rem perspiciatis odio.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quod itaque
+      cupiditate aperiam perferendis. Alias ad esse assumenda, mollitia vero
+      veritatis architecto sapiente, expedita in, voluptatibus hic omnis?
+      Necessitatibus, aliquid temporibus. Nihil, aperiam quod. Eum iste sequi
+      amet officia natus, iusto error distinctio aliquid facere, quibusdam
+      consectetur assumenda nostrum voluptates?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
