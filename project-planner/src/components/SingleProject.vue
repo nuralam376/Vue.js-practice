60394467fa6b4bc07b58525aecf828f5938f84cb
Nur A Alam <nuraalam376@gmail.com>
2021-02-22 09:24:22 +0600

    started learning vue.js project-planner

---
+++ project-planner/src/components/SingleProject.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 @click="showDetails = !showDetails">{{ project.title }}</h1>
+  <div class="details" v-if="showDetails">
+    {{ project.details }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      showDetails: false,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ project-planner/src/views/Projects.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Projects</h1>
+  <div v-if="projects.length">
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project="project" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleProject from "../components/SingleProject";
+export default {
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
+};
+</script>
+
+<style></style>
