659b5838110b6b4bdfd478dfa54eb70afcc5fd97
Nur A Alam <nuraalam376@gmail.com>
2021-02-23 09:01:59 +0600

    practiced vue.js delete

--- project-planner/src/components/SingleProject.vue
+++ project-planner/src/components/SingleProject.vue
@@ -1,7 +1,21 @@
 <template>
-  <h1 @click="showDetails = !showDetails">{{ project.title }}</h1>
-  <div class="details" v-if="showDetails">
-    {{ project.details }}
+  <div class="project" :class="{ complete: project.complete }">
+    <div class="actions">
+      <h3 @click="showDetails = !showDetails">
+        {{ project.title }}
+      </h3>
+      <div class="icons">
+        <div class="material-icons">edit</div>
+        <div class="material-icons" @click="deleteProject">delete</div>
+        <div
+          class="material-icons"
+          @click="completeProject"
+          :class="{ tick: project.complete }"
+        >
+          done
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,9 +25,64 @@ export default {
   data() {
     return {
       showDetails: false,
+      uri: "http://localhost:3000/projects/" + this.project.id,
     };
+  },
+
+  methods: {
+    deleteProject() {
+      fetch(this.uri, { method: "DELETE" })
+        .then(() => this.$emit("delete", this.project.id))
+        .catch((err) => console.log(err));
+    },
+
+    completeProject() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ complete: !this.project.complete }),
+      }).then(() => this.$emit("complete", this.project.id));
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+.project {
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);
+  border-left: 4px solid #e90074;
+}
+
+h3 {
+  cursor: pointer;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.material-icons {
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+
+.material-icons:hover {
+  color: #777;
+}
+
+.project.complete {
+  border-left-color: green;
+}
+
+.project.complete .tick {
+  color: green;
+}
+</style>
--- project-planner/src/views/Projects.vue
+++ project-planner/src/views/Projects.vue
@@ -2,7 +2,11 @@
   <h1>Projects</h1>
   <div v-if="projects.length">
     <div v-for="project in projects" :key="project.id">
-      <SingleProject :project="project" />
+      <SingleProject
+        :project="project"
+        @delete="handleDelete"
+        @complete="handleComplete"
+      />
     </div>
   </div>
 </template>
@@ -22,6 +26,18 @@ export default {
       .then((res) => res.json())
       .then((data) => (this.projects = data))
       .catch((err) => console.log(err.message));
+  },
+
+  methods: {
+    handleDelete(id) {
+      console.log("id", id);
+      this.projects = this.projects.filter((project) => project.id !== id);
+    },
+
+    handleComplete(id) {
+      let project = this.projects.find((pj) => pj.id === id);
+      project.complete = !project.complete;
+    },
   },
 };
 </script>
