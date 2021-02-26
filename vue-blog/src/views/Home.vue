1d84860108836e6d2d0a3790cf42263db9a22f02
Nur A Alam <nuraalam376@gmail.com>
2021-02-26 09:14:20 +0600

    started learning composition api

---
+++ vue-blog/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <h1>Composition API</h1>
+    <p ref="p">{{ name }} {{ age }}</p>
+    <button @click="handleClick">Button</button>
+    <input type="text" v-model="name" />
+    <button @click="age++">Increase age</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "Home",
+  components: {},
+
+  setup() {
+    console.log("Setup'");
+
+    const name = ref("Abc");
+    const age = ref(45);
+
+    const handleClick = () => {
+      console.log("button clicked");
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "hello";
+    };
+
+    const p = ref(null);
+
+    return { name, age, handleClick, p };
+  },
+
+  created() {
+    console.log("Created");
+  },
+
+  mounted() {
+    console.log("Mounted");
+  },
+
+  updated() {
+    console.log("Updated");
+  },
+};
+</script>
