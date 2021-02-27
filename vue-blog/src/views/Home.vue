17b21e999071b9c34725c6c4b7a4e1433340ead6
Nur A Alam <nuraalam376@gmail.com>
2021-02-27 09:42:30 +0600

    practiced vue.js watch and watchEfftc

--- vue-blog/src/views/Home.vue
+++ vue-blog/src/views/Home.vue
@@ -1,82 +1,46 @@
 <template>
   <div class="home">
-    <h1>Composition API</h1>
-    <p ref="p">{{ name }} {{ age }}</p>
-    <button @click="handleClick">Button</button>
-    <input type="text" v-model="name" />
-    <button @click="age++">Increase age</button>
-
-    <br />
-    <h3>{{ person.name }} - {{ person.age }} - {{ anotherName }}</h3>
-    <button @click="updatePerson">Update Person Age</button>
-
-    <br />
-
-    <h3>{{ person2.name }} - {{ person2.age }} - {{ newName }}</h3>
-    <button @click="updatePerson2">Update Person 2</button>
+    <input type="text" v-model="search" />
+    <p>{{ search.value }}</p>
+    <p v-for="name in matchingNames" :key="name">{{ name }}</p>
+    <button @click="handleEffect">Stop Watch</button>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 export default {
   name: "Home",
   components: {},
 
   setup() {
-    console.log("Setup'");
+    const search = ref("");
+    const names = ref(["Abc", "Def", "Ghi", "Jkl", "Mno"]);
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
 
-    let name = ref("Abc");
-    let age = reactive(45);
-    const person = reactive({ name: "Def", age: 30 });
-    const person2 = ref({ name: "Abc", age: 20 });
+    const stopWatch = watch(search, () => {
+      console.log("Watch function ran");
+    });
 
-    let newName = ref("test");
-    let anotherName = reactive("test2");
+    const stopWatchEffect = watchEffect(() => {
+      console.log("Watch Effect function ran", search.value);
+    });
 
-    const updatePerson = () => {
-      person.age = 40;
-      newName = "s";
+    const handleEffect = () => {
+      stopWatch();
+      stopWatchEffect();
     };
-
-    const updatePerson2 = () => {
-      person2.value.age = 30;
-      anotherName = "test22";
-    };
-
-    const handleClick = () => {
-      console.log("button clicked");
-      console.log(p, p.value);
-      p.value.classList.add("test");
-      p.value.textContent = "hello";
-    };
-
-    const p = ref(null);
 
     return {
-      name,
-      age,
-      handleClick,
-      p,
-      person,
-      updatePerson,
-      person2,
-      updatePerson2,
-      anotherName,
-      newName,
+      names,
+      search,
+      matchingNames,
+      stopWatch,
+      stopWatchEffect,
+      handleEffect,
     };
-  },
-
-  created() {
-    console.log("Created");
-  },
-
-  mounted() {
-    console.log("Mounted");
-  },
-
-  updated() {
-    console.log("Updated");
   },
 };
 </script>
