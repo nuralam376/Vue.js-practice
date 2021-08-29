1a7d558fe843604d871bbc6a36965c896b8a94e5
Nur A Alam <nuraalam376@gmail.com>
2021-08-29 07:53:32 +0600

    practiced watcher

---
+++ nuxt-js/nuxt-crash/pages/practice.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <h1>Volume - {{ volume }}</h1>
+
+      <button @click = "volume += 2">Increase</button>
+      <button @click = "volume -= 2">Decrease</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data()
+    {
+        return {
+            volume : 0
+        };
+    },
+
+    watch : {
+        volume(newValue, oldValue)
+        {
+            if(newValue > oldValue && newValue === 16)
+            {
+                alert("Volume is too high");
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
